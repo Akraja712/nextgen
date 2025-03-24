8a5f171d66c9221fb861406e02073b141ffa5d50
Akraja712 <ashokkumar7hitter@gmail.com>
2025-03-24 18:15:53 +0530

    crud.php

--- includes/connection.php
+++ includes/connection.php
@@ -1,6 +1,6 @@
 <?php
- define('DOMAIN_URL', 'https://demonextgen.enlightapp.in/');
- define('API_URL', 'https://demonextgen.enlightapp.in/admin_v1/api/');
+ define('DOMAIN_URL', 'https://nextgen.enlightapp.in/');
+ define('API_URL', 'https://nextgen.enlightapp.in/admin_v1/api/');
 //  define('DOMAIN_URL', 'https://demo.solarpe.org/');
 //  define('API_URL', 'https://demo.solarpe.org/main_controlv3/api/');
 ?>
